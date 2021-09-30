ce0d4993b7f0222dbbd452254690e666a2e8fe5c
Han Yi <44139942+HanYiChoong@users.noreply.github.com>
2021-09-30 15:11:22 +0800

    Resolved merge conflict with checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import LoginPage from './pages/login';
-import RegisterPage from './pages/register'
-import ProductsPage from "./pages/products"
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-import Cart from './cart/Cart';
-import CheckoutPage from './pages/Checkout';
+import RegisterPage from './pages/register';
+import ProductsPage from "./pages/products";
+import CartPage from './pages/cart';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -13,29 +12,9 @@ function App() {
           <div className="container">
             {/* <Route exact path="/" component={RegisterPage} /> */}
             <Route exact path="/register" component={RegisterPage} />
-            <Route exact path="/login" render={() => (
-              localStorage.usertoken && localStorage.usertoken !== 'undefined' ? (
-                <Redirect to="/" />
-              ) : (
-                <LoginPage />
-              )
-            )} />
-
-            <Route exact path="/checkout" render={() => (
-              localStorage.usertoken && localStorage.usertoken !== 'undefined' ? (
-                <CheckoutPage />
-              ) : (
-                <Redirect to="/login" />
-              )
-            )} />
-
-            <Route exact path="/" render={() => (
-              localStorage.usertoken && localStorage.usertoken !== 'undefined' ? (
-                <ProductsPage />
-              ) : (
-                <Redirect to="/login" />
-              )
-            )} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/" component={ProductsPage} />
+            <Route exact path="/cart" component={CartPage} />
           </div>
       </Router>
     </div>
