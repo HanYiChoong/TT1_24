d3eac25ea9ccbdecccf55e83ae98df374ca4e3dc
zhiyi <ongzhiyi@live.com>
2021-09-30 12:48:47 +0800

    bugged

---
+++ frontend/src/pages/CheckoutPage.js
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const CheckoutPage = () => {
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Typography variant='h6'>
+                    some words
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default CheckoutPage
